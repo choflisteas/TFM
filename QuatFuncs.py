f00749568549e169f62b48c31062eb1ed09f85b9
Gonzalo <gh.vara@gmail.com>
2016-07-15 12:00:59 +0200

    Update QuatFuncs.py

--- QuatFuncs.py
+++ QuatFuncs.py
@@ -1,8 +1,8 @@
 import math
 from mathutils import Quaternion
 
-import numpy as np
-import matplotlib.pyplot as plt
+#import numpy as np
+#import matplotlib.pyplot as plt
 import mpl_toolkits.mplot3d.axes3d as p3
 import matplotlib.animation as animation
 
@@ -13,8 +13,6 @@ from matplotlib.backends.backend_qt4agg import (
     NavigationToolbar2QT as NavigationToolbar)
 
 import threading
-
-import random #solamente usado para hacer las pruebas del plot
 
 def getGlobalAngles(q0):
     ang = [0.0, 0.0, 0.0]
@@ -38,16 +36,6 @@ def getAnglesFromQuaternions(q):
         for ax in axis:
             ang[frame][axis] = math.degrees(q[frame][0].to_euler()[i])
     return ang
-    
-def getEulerAngles(q0):
-    ang = [0.0,0.0,0.0]
-    v = Quaternion((0,0,0,1))
-    q = q0*v*q0.conjugated()
-
-    for i in range(0,3):
-        ang[i] = math.degrees(q.to_euler()[i])
-    
-    return ang
 
 def getDifAngles(q0,q1):
     angA = [0.0, 0.0, 0.0]
@@ -68,100 +56,257 @@ def getDifAngles(q0,q1):
     
     return [sum(i) for i in zip(angA,angB)]
     
-
-    
-#-----------------------------------------------------------------------------#
-    
-class tempGraph():
-    def __init__(self, kinem_chain, name, frames, axis, inf_limit, sup_limit):
-        # kinem_chain es un objeto de la clase 'kinematic_chain' que contiene los datos a representar
-        # inf_limit and sup limit are the values used to plot the red lines
-        # widget is where the graph will be plotted
-        self.name = name
-        self.chain = kinem_chain
-        lim = self.defLimits(inf_limit, sup_limit)
-        fra = self.__checkFrames(frames)
-        ax = self.__checkAxis(axis)
-        if lim and fra and ax:
-            print (">>> La gráfica %s se ha creado correctamente. Mostrará el ángulo entre \
-%s y %s en el eje %s" %(self.name,self.frames[0], self.frames[1], self.axis))
-        else:
-            print ("-->No se ha podido crear la gráfica %s." %name)
-    
-    def defLimits(self, inf_limit,sup_limit):
-        if inf_limit<-180 or sup_limit>180 or sup_limit<inf_limit:
-            raise ValueError(">>> ERROR: Los límites introducidos no son válidos", self.name )
-        else:
-            self.inf_limit = inf_limit
-            self.sup_limit = sup_limit
-            return True
-    
-    def __checkFrames(self, frames):
-        # el número de frames debe ser 2.
-        if len(frames) != 2:
-            print(">>> ERROR: El número de eslabones no es correcto")
-            return False
-        # las frames indicadas deben estar en la base de datos del objeto kinem_chain
-        for frame in frames:
-            if frame not in self.chain.ang:
-                print(">>> ERROR: No se han encontrado los eslabones en la estructura de datos")
-                return False
-        self.frames = frames
-        return True
+#------------------------------------------------------------------------------
         
-    def __checkAxis(self,axis):
-        if axis not in ['X','Y','Z','x','y','z']:
-            return False
-        else:
-            self.axis = str.capitalize(axis)
-            return True
-    
-    def setColor(self,color='blue'):
-        if color not in ['blue', 'green', 'red', 'black', 'orange', 'yellow']:
-            return False
-        else:
-            self.color=color      
-
-    def updatePlot(self,i):
-        
-        # Actualizar los datos
-        if len(self.chain.ang) == 0: #Esto se produciría cuando aún no se hubiera calculado ningún ángulo        
-            pass
-        else:
-            self.angle.pop(0)
-            ang = self.chain.ang[self.frames[0]][self.axis]-self.chain.ang[self.frames[1]][self.axis]
-            #ang = random.random()*10            
-            print ("Dato nuevo %f" %ang)
-            self.angle.append(ang)
-            self.line.set_ydata(self.angle)
-        
-
-    def plotGraph(self, widget):
+class plot3DChain:
+    def __init__(self, chain, layout):
         # 'widget' será dónde se quiera mostrar el gráfico
+        # 'chain' es la cadena cinemática a representar
+        self.chain = chain
+        # Variable para controlar la representación gráfica del objeto
+        self.plotting = True
         # Creación de la figura
         self.fig = Figure()
         # y enlace con el canvas.
         self.canvas = FigureCanvas(self.fig)
+        # se hace propio el layout para poder ocultar/mostrar el gráfico
+        self.layout = layout        
         # Creación los ejes 3D
-        self.ax = self.fig.add_subplot('111')
+        self.ax3d = p3.Axes3D(self.fig)
         # Establecimiento de los límites de los ejes
-        self.ax.set_ylim(self.inf_limit, self.sup_limit)
-        self.ax.set_title(self.name)
+        self.ax3d.set_xlim3d([-0.5, 0.5])
+        self.ax3d.set_xlabel('X')
+        self.ax3d.set_ylim3d([-0.5, 0.5])
+        self.ax3d.set_ylabel('Y')
+        self.ax3d.set_zlim3d([0.0, 1.0])
+        self.ax3d.set_zlabel('Z')
+        self.ax3d.set_title('3D View')     
         
-        self.setColor()
-        
-        self.angle = []
+        # Creación de la lista de colores
+        col = ['red','blue','green', 'black', 'yellow', 'pink', 'orange']  
+        colors = {}
+        for i in range(0,self.chain.nframes):
+            j = i
+            if i>6:
+                j=i%6
+            colors[self.chain.frames[i]] = col[j]
        
-        for i in range(0,100):
-            self.angle.append(0)
-        self.line, = self.ax.plot(self.angle, color=self.color)
+        # Definición de las coordenadas iniciales ya que no se pueden crear lineas vacías.
+        coordinates = []
+        for j in range(0,self.chain.nframes): #para cada uno de los eslabones        
+            coordinates.append([])
+            for i in range(0,3): #para cada uno de los ejes
+                coordinates[j].append((0,0))
 
-        self.ax.hold(False)                        
-        # Enlace del canvas con el widget
-        widget.addWidget(self.canvas)
+        #coordinates = [((0,0),(0,0),(0,0)),((0,0),(0,0),(0,0)),((0,0),(0,0),(0,0))]
+        coordinates = dict(list(zip(self.chain.frames, coordinates)))
+
+        # Creación de las 3DLines
+        self.lines = {}
+        for name in self.chain.frames:
+            self.lines[name] = self.ax3d.plot(coordinates[name][0],coordinates[name][1],coordinates[name][2],'-o',markersize=4,markerfacecolor="orange",linewidth=3, color=colors[name])
+        # Se llama a ax.hold para que borre lo que estaba antes y dibuje todo nuevo.                            
+        self.ax3d.hold(False)                        
+        layout.addWidget(self.canvas) #con esta opción se inserta en un layout.
+        #self.canvas3d.setParent(widget)
         self.canvas.draw()
         # Creación de la animación.
-        self.anim = animation.FuncAnimation(self.fig, self.updatePlot,interval=100, blit=False)
+        self.anim = animation.FuncAnimation(self.fig, self.updatePlot,interval=100, blit=False)   
+
+    def updatePlot(self,i):
+        # Se comprueba si la visualización está activada:
+        if self.plotting == True:
+
+        # y se actualizan los valores de las lineas:
+            for name in self.chain.frames:
+                self.lines[name][0].set_data(self.chain.coordinates[name][0], self.chain.coordinates[name][1])
+                self.lines[name][0].set_3d_properties(self.chain.coordinates[name][2])
+
+    def togglePlot(self):
+        self.plotting = not self.plotting
+
+#------------------------------------------------------------------------------
+
+class chainGraphs():
+    def __init__(self, kinem_chain, layout):
+        # kinem_chain es un objeto de la clase 'kinematic_chain' que contiene los datos a representar
+        # inf_limit and sup limit are the values used to plot the red lines
+        # widget is where the graph will be plotted
+        self.chain = kinem_chain
+
+        # Variable para controlar la representación gráfica del objeto
+        self.plotting = True
+        
+        self.layout = layout
+        self.__initData()
+        self.__initPlots()
+
+ 
+        self.plotThread = recurringTimer(0.1, self.updatePlots)
+        self.plotThread.start_timer()
+        
+    def __initData(self):
+        # Articulación que se muestra por defecto:
+        self.activeJoint = self.chain.joints[0] #por defecto
+        # Datos para este ejemplo en concreto:
+        self.graphData = {'munyeca':{'X':{'name':'Desviación radial(-)/cubital(+)','max':15,'min':-20,'plot':True,'offset':0},
+                        'Y':{'name':'Flexión(+)/Extensión(-)','max':45,'min':-45,'plot':True,'offset':0},
+                        'Z':{'name':' ','max':30,'min':-10,'plot':False,'offset':0}},
+                        'codo':{'X':{'name':'Flexión(-)/Extensión(+)','max':120,'min':60,'plot':True,'offset':0},
+                        'Y':{'name':'','max':40,'min':-35,'plot':False,'offset':0},
+                        'Z':{'name':'Pronación(-)/Supinación(+)','max':60,'min':-60,'plot':True,'offset':0}},
+                        'hombro':{'X':{'name':'Flexión(-)/Extensión(+)','max':10,'min':-10,'plot':True,'offset':0},
+                        'Y':{'name':'Abducción(+)/Adducción(-)','max':45,'min':-20,'plot':True,'offset':0},
+                        'Z':{'name':'Rotación int.(-)/ext(+)','max':30,'min':-30,'plot':True,'offset':0}}}    
+    
+    def checkJointAngles(self,joint,axis):
+        if self.plotData[joint][axis][-1]>self.graphData[joint][axis]['max'] or\
+        self.plotData[joint][axis][-1]<self.graphData[joint][axis]['min']:
+            return True
+        else:
+            return False                 
+        
+    
+    def getCurrentJoint(self):
+        pass
+    
+    def togglePlot(self):
+        self.plotting = not self.plotting
+        if self.plotting == True:
+            self.plotThread.start_timer()
+        else:
+            self.plotThread.stop_timer()
+
+    def updatePlots(self):
+        # Actualizar los datos si self.plotting está activado
+        if self.plotting == True:
+            for joint in self.chain.joints:
+                for axis in ['X','Y','Z']:
+                    self.plotData[joint][axis].pop(0)
+                    self.plotData[joint][axis].append(self.chain.ang[joint][axis]+self.graphData[joint][axis]['offset'])
+                
+        for axis in ['X','Y','Z']:
+            if self.firstPlot == True:
+                self.firstPlot = False
+                self.selectJoint('hombro')
+            
+            if self.graphData[self.activeJoint][axis]['plot'] == False:
+                pass
+            else:
+                self.plotStuff[axis]['line'].set_ydata(self.plotData[self.activeJoint][axis])
+    
+                self.plotStuff[axis]['ax'].draw_artist(self.plotStuff[axis]['ax'].patch)
+                self.plotStuff[axis]['ax'].draw_artist(self.plotStuff[axis]['line'])
+                self.plotStuff[axis]['ax'].draw_artist(self.plotStuff[axis]['limits'])
+    
+                if (self.checkJointAngles(self.activeJoint,axis)==True) and (self.plotStuff[axis]['ax'].get_axis_bgcolor() is not 'r'):
+                    self.plotStuff[axis]['ax'].set_axis_bgcolor('r')
+                elif (self.checkJointAngles(self.activeJoint,axis)==False) and (self.plotStuff[axis]['ax'].get_axis_bgcolor()is not 'w'):
+                    self.plotStuff[axis]['ax'].set_axis_bgcolor('w')
+                
+                self.plotStuff[axis]['fig'].canvas.update()
+                self.plotStuff[axis]['fig'].canvas.flush_events()
+                   
+
+    def __initPlots(self):
+
+        initLine = []
+        for i in range (0,100):
+            initLine.append(0)
+        
+        colors = {'X':'green','Y':'blue','Z':'orange'}        
+        
+        self.plotStuff = {}           
+        for axis in ['X','Y','Z']:
+            self.plotStuff[axis] = {}
+            self.plotStuff[axis]['fig'] = Figure()
+            self.plotStuff[axis]['canvas'] = FigureCanvas(self.plotStuff[axis]['fig'])
+            self.plotStuff[axis]['ax'] = self.plotStuff[axis]['fig'].add_subplot('111')
+            self.plotStuff[axis]['ax'].hold(False)            
+            self.plotStuff[axis]['line'], = self.plotStuff[axis]['ax'].plot(initLine, color = colors[axis], linewidth = 2)
+            minAngle = self.graphData[self.activeJoint][axis]['min']
+            maxAngle = self.graphData[self.activeJoint][axis]['max']
+            self.plotStuff[axis]['limits'] = self.plotStuff[axis]['ax'].hlines([minAngle,maxAngle],0,99,colors = 'red',linestyles='dashed')
+            
+            self.layout.addWidget(self.plotStuff[axis]['canvas'])
+            self.plotStuff[axis]['canvas'].draw()
+            #self.plotStuff[axis]['ax'].set_ylim(-180,180)
+            
+            self.firstPlot = True
+            
+            #title = 'Eje'+axis
+            #self.plotStuff[axis]['ax'].set_title(title)
+            
+        self.plotData = {}
+        for joint in self.chain.joints:
+            self.plotData[joint]={}
+            self.plotData[joint]['plotting'] = False
+            for axis in ['X', 'Y', 'Z']:
+                self.plotData[joint][axis] = initLine.copy()
+
+    def __changePlotLimits(self):
+        for axis in ['X','Y','Z']:
+            minAngle = self.graphData[self.activeJoint][axis]['min']
+            maxAngle = self.graphData[self.activeJoint][axis]['max']
+            self.plotStuff[axis]['limits'] = self.plotStuff[axis]['ax'].hlines([minAngle,maxAngle],0,99,colors = 'red',linestyles='dashed')
+            
+    def __hideSomePlots(self):
+        for axis in ['X','Y','Z']:
+            if self.graphData[self.activeJoint][axis]['plot'] == False:
+                self.plotStuff[axis]['ax'].set_axis_bgcolor('grey')
+                self.plotStuff[axis]['ax'].clear()
+            else:
+                self.plotStuff[axis]['ax'].set_ylim(-180,180)
+    
+    def __changePlotTitles(self):
+        for axis in ['X','Y','Z']:
+            title = self.graphData[self.activeJoint][axis]['name']
+            title = self.plotStuff[axis]['ax'].set_title(title)
+            self.plotStuff[axis]['canvas'].draw()
+
+    def selectJoint(self,joint):
+        if joint not in self.chain.joints:
+            print("La articulación no se encuentra.")
+            return False
+        else:
+            self.activeJoint = joint
+            self.__changePlotLimits()
+            self.__hideSomePlots()
+            self.__changePlotTitles()
+            return True
+        
+#------------------------------------------------------------------------------
+        
+class recurringTimer(threading.Timer):
+    # Implementación de una clase que permite la invocación de un método de forma periódica
+     
+    def __init__ (self, *args, **kwargs):
+        threading.Timer.__init__ (self, *args, **kwargs) 
+        self.setDaemon (True)
+        self._running = 0
+        self._destroy = 0
+        self.start()
+ 
+    def run (self):
+        while True:
+            self.finished.wait (self.interval)
+            if self._destroy:
+                return;
+            if self._running:
+                self.function (*self.args, **self.kwargs)
+ 
+    def start_timer (self):
+        self._running = 1
+ 
+    def stop_timer (self):
+        self._running = 0
+ 
+    def is_running (self):
+        return self._running
+ 
+    def destroy_timer (self):
+        self._destroy = 1;
+
         
     
     
